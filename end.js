baf7a74d0a3c41f27dde0b7f7c519a0237fdea5c
KazM7132 <150427004+KazM7132@users.noreply.github.com>
2023-12-13 12:02:14 +0400

    Update end.js

--- end.js
+++ end.js
@@ -2,7 +2,7 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
-finalScore.innerText = `${mostRecentScore}/10`;
+finalScore.innerText = `${mostRecentScore}/3`;
 
 username.addEventListener('keyup', () => {
     saveScoreBtn.disabled = !username.value;
